5f76e40217aa8a4290aee5ba7308efac05ba42da
patel-tirth <pateltirth1750@gmail.com>
2020-08-18 22:25:33 -0400

    Updates

---
+++ precache-manifest.a194902dfbfa648a8fd70df2e66b2370.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bd23ecb2f7c38ea5df1de8deb160360",
+    "url": "/ReactTicTacToe/index.html"
+  },
+  {
+    "revision": "8f7ff87f207c893bb02a",
+    "url": "/ReactTicTacToe/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "dbdf6094b99a074ddd8a",
+    "url": "/ReactTicTacToe/static/js/2.094fd168.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactTicTacToe/static/js/2.094fd168.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f7ff87f207c893bb02a",
+    "url": "/ReactTicTacToe/static/js/main.2344bc8c.chunk.js"
+  },
+  {
+    "revision": "cfc32f3e87c9b3708434",
+    "url": "/ReactTicTacToe/static/js/runtime-main.6558fa35.js"
+  }
+]);
